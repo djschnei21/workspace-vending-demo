cf2381a9312b7c67c3342ca8c28e9f7fe61f2f57
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-13 16:19:29 +0000

    create variable set

--- main.tf
+++ main.tf
@@ -14,3 +14,47 @@ module "workspace-vending" {
   ]
   app_envs = [ "development", "uat", "production" ]
 }
+
+variable "dev_vsphere_server" {
+  type = string
+  default = ""
+}
+
+variable "dev_vsphere_password" {
+  type = string
+  default = ""
+  sensitive = true
+}
+
+variable "dev_vsphere_user" {
+  type = string
+  default = ""
+}
+
+resource "tfe_variable_set" "dev_vsphere" {
+  name          = "Dev vSphere Credentials"
+  description   = "Required variables for a workspace to connect to dev vSphere"
+  organization  = "djs-tfcb"
+}
+
+resource "tfe_variable" "dev_vsphere_server" {
+  key             = "vsphere_server"
+  value           = var.dev_vsphere_server
+  category        = "terraform"
+  variable_set_id = tfe_variable_set.dev_vsphere.id
+}
+
+resource "tfe_variable" "dev_vsphere_password" {
+  key             = "vsphere_password"
+  value           = var.dev_vsphere_password
+  category        = "terraform"
+  sensitive       = true
+  variable_set_id = tfe_variable_set.dev_vsphere.id
+}
+
+resource "tfe_variable" "dev_vsphere_user" {
+  key             = "vsphere_user"
+  value           = var.dev_vsphere_user
+  category        = "terraform"
+  variable_set_id = tfe_variable_set.dev_vsphere.id
+}
