1ab0a0d4ff94cbb671c2aaa3c045c3693929a621
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-21 12:31:00 +0000

    github source for testing

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@ terraform {
 }
 
 module "workspace-vending" {
-  source  = "app.terraform.io/djs-tfcb/workspace-vending/tfe"
-  version = "4.0.0"
+  # source  = "app.terraform.io/djs-tfcb/workspace-vending/tfe"
+  # version = "4.0.0"
+  source = "github.com/djschnei21/terraform-tfe-workspace-vending"
 
   tf_org = "djs-tfcb"
   gh_org = "djschnei21"
